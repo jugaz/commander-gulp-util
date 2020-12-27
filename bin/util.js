b9f7aed40d56221cbd247497e0e61af8e058cc37
Jesús Antonio Ugaz Vegas <jesusugazv@gmail.com>
2020-12-26 19:32:02 -0500

    fix:update files

--- bin/util.js
+++ bin/util.js
@@ -3,6 +3,7 @@
 var
     debug = require('gulp-debug'),
     fs = require('fs'),
+    path = require('path'),
     named = require('vinyl-named'),
     clean = require('del'),
     program  = require('commander'),
@@ -23,7 +24,7 @@ program
     )
 
 /* ######################## GULP UTIL ###################### */
-// example node util.js util 'svg/' --ut 'build/'
+// example node .bin/util.js util \"dist/\"",
 
 program
     .command('util <dir>')
@@ -31,39 +32,41 @@ program
     .action((input, options) => {
         var input = options.input || options.parent.rawArgs;
         var ouput = options.ouput || options.ut;
-        var files = []
-        function error(){
-            util.log("No existe la carpeta o carpetas")
-        }
+  
+        var directory = [];
         input = input.forEach(element => {
             if (fs.existsSync(element)) {
-                var stat = fs.statSync(element);
-                if (stat.isDirectory() && element !== ouput) {
-                    files.push(element)
+                var dir = fs.statSync(element);
+                var $true = dir.isDirectory();
+                var $true2 = dir.isDirectory() === true;
+                if ($true2  && $true !=='/home/jugaz/.nvm/versions/node/v15.4.0/bin/node' && $true !== '/home/jugaz/Escritorio/Developer/.Github/commander-gulp-util/bin/util.js') {
+                    return directory.push(element)
                 }
-                else if(stat.isDirectory()=="undefine" || stat.isDirectory()=="" || stat.isDirectory() !=element ) {
-                    return error()
-                }
-              }
+            }
         });
-        
-        return src(files, { allowEmpty: true })
-            .pipe(debug({
-                title: 'commader-gulp-util:'
-            }))
-            .on('error', function (error) {
-                // tenemos un error 
-                util.log("Error Name:", error.name);
-                util.log("Error Code:", error.code);
-                util.log("Error Filename:", error.filename);
-                util.log("Error Line:", error.line);
-                util.log("Error Column:", error.column);
-                util.log("Error Msg", error.Msg);
-            })
-            .pipe(dest(ouput))
-            .on('end', function () {
-                util.log('Done!');
-            });
+        if(directory.length === 0 || directory === "undefine") {
+            return util.log("No existe la carpeta o carpetas")
+        }
+        else {
+            return src(files, { allowEmpty: true })
+                .pipe(debug({
+                    title: 'commader-gulp-util:'
+                }))
+                .on('error', function (error) {
+                    // tenemos un error 
+                    util.log("Error Name:", error.name);
+                    util.log("Error Code:", error.code);
+                    util.log("Error Filename:", error.filename);
+                    util.log("Error Line:", error.line);
+                    util.log("Error Column:", error.column);
+                    util.log("Error Msg", error.Msg);
+                })
+                .pipe(dest(ouput))
+                .on('end', function () {
+                    util.log('Done!');
+                });
+        }
+       
     })
 
 /* ######################## GULP CLEAN ###################### */
@@ -72,49 +75,56 @@ program
     .command('clean <dir>')
     .action((input, options) => {
         var input = options.input || options.parent.rawArgs;
-        var files = []
-        function error(){
-            util.log("No existe la carpeta o carpetas")
-        }
-
+        var directory = [];
         input = input.forEach(element => {
             if (fs.existsSync(element)) {
-                var stat = fs.statSync(element);
-                
-                if (stat.isDirectory()) {
-                    return files.push(element)
+                var dir = fs.statSync(element);
+                var $true = dir.isDirectory();
+                var $true2 = dir.isDirectory() === true;
+                if ($true2  && $true !=='/home/jugaz/.nvm/versions/node/v15.4.0/bin/node' && $true !== '/home/jugaz/Escritorio/Developer/.Github/commander-gulp-util/bin/util.js') {
+                    return directory.push(element)
                 }
-        
-                else if(stat.isDirectory()=="undefine" || stat.isDirectory()=="" || stat.isDirectory() !=element ) {
-                    return error()
-                }
-                
-              }
+            }
         });
-        return src(files, { allowEmpty: true })
-            .pipe(debug({
-                title: 'commader-gulp-util:'
-            }))
-            .pipe(named())
-            .pipe(webpack({
-                watch: false
-            }))
-  
-            .pipe(dest(clean(files,{force:true})))
+ 
+     
+    
+     
+        if(directory.length === 0 || directory === "undefine") {
+            return util.log("No existe la carpeta o carpetas")
+        }
+        else {
+            return src(directory, { allowEmpty: true })
+                .pipe(debug({
+                    title: 'commader-gulp-util:'
+                }))
+                .pipe(named())
+                .pipe(webpack({
+                    watch: false
+                }))
 
-            .on('error', function (error) {
-                // tenemos un error 
-                util.log("Error Name:", error.name);
-                util.log("Error Code:", error.code);
-                util.log("Error Filename:", error.filename);
-                util.log("Error Line:", error.line);
-                util.log("Error Column:", error.column);
-                util.log("Error Msg", error.Msg);
-            })
-            .on('end', function () {
-                util.log('Done!');
-            });
-        })
+                .pipe(dest(clean(directory,{force:true})))
+
+                .on('error', function (error) {
+                    // tenemos un error 
+                    if(directory ==="" ) {
+                    
+                    }
+                    else {
+                        util.log("Error Name:", error.name);
+                        util.log("Error Code:", error.code);
+                        util.log("Error Filename:", error.filename);
+                        util.log("Error Line:", error.line);
+                        util.log("Error Column:", error.column);
+                        util.log("Error Msg", error.Msg);
+                    }
+                })
+                .on('end', function () {
+                    util.log('Done!');
+                });
+        }
+       
+})
         
         
         
